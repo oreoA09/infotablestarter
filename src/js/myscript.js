51ea763835ff7b4bda44faf4d59217f3a3cf2a7f
Henry Ebomah <henryebomah@gmail.com>
2021-08-01 13:14:36 -0700

    taskfinal

--- src/js/myscript.js
+++ src/js/myscript.js
@@ -1,25 +1,26 @@
 console.log('Welcome to HackerU-JustCode Assignment');
-$(document).ready(function() {
+$(document).ready(function () {
     getRemoteData();
 });
 function getRemoteData() {
     // write your code here to fetch data from api. 
     fetch('https://www.justcode.com/dataservice/api/paymentMethod/list')
-    .then(response => response.json())
-    .then(responseJson => 
-        displayResults(responseJson))   
+        .then(response => response.json())
+        .then(responseJson =>
+            displayResults(responseJson))
         .catch(err => {
-         console.log(err)
-        $('#js-error-message').text(`Something went wrong: ${err.message}`);
+            console.log(err)
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
         });
-    }
-            
+}
+
 
 function displayResults(responseJson) {
     console.log(responseJson);
+    //reads the data from the fetch and creates html
     $("#mytable").html("")//clears the results
-    responseJson.forEach(table=>{//loops through the array
-      $("#mytable").append(`
+    responseJson.forEach(table => {//loops through each data in the array
+        $("#mytable").append(`
       <tr>
       <th>${table.id}</th>
       <th>${table.methodName}</th>
@@ -29,17 +30,18 @@ function displayResults(responseJson) {
       </tr>
       `)//appends table to results div
     })
-   }
+}
 
 
 function watchForm() {
     $('#mytable').submit(event => {
-      event.preventDefault();
-      getRemoteData();
+    //clears the results
+        event.preventDefault();
+        getRemoteData();
     });
-  }
-  
-  $(function() {
+}
+//run this function 1st anytime the app starts or the document is ready
+$(function () {
     console.log('App loaded! Waiting!');
     watchForm();
-  });
+});
