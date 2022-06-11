3df19dff293270b1039e3170d1cc1fbe68291ee6
oreoA09 <oreo.akanni@gmail.com>
2022-06-11 11:24:34 +0000

    first commit

--- src/js/myscript.js
+++ src/js/myscript.js
@@ -4,41 +4,37 @@ $(document).ready(function () {
 });
 function getRemoteData() {
     // write your code here to fetch data from api. 
-    fetch('https://www.justcode.com/dataservice/api/paymentMethod/list')
-        .then(response => response.json())
-        .then(responseJson =>
-            displayResults(responseJson))
-        .catch(err => {
-            console.log(err)
-            $('#js-error-message').text(`Something went wrong: ${err.message}`);
-        });
+   fetch(`https://www.justcode.com/dataservice/api/paymentMethod/list`)
+   .then(response => response.json())
+   .then(responseJson => displayResults(responseJson))
 }
 
-
-function displayResults(responseJson) {
-    console.log(responseJson);
-    //reads the data from the fetch and creates html
-    $("#mytable").html("")//clears the results
-    responseJson.forEach(table => {//loops through each data in the array
-        $("#mytable").append(`
-      <tr>
-      <th>${table.id}</th>
-      <th>${table.methodName}</th>
-      <th>${table.medium}</th>
-      <th>${table.yearOfOrigin}</th>
-      <th>${table.modifiedOn}</th>
-      </tr>
-      `)//appends table to results div
+function displayResults(responseJson){
+     console.log(responseJson);
+    $('#mytable').html("")
+    responseJson.forEach(table => {
+        $('#mytable').append(`
+        <div>
+        <table>
+        <tr>
+        <th>${table.id}</th>
+        <th>${table.methodName}</th>
+        <th>${table.medium}</th>
+        <th>${table.yearOfOrigin}</th>
+        <th>${table.modifiedOn}</th>
+        </tr>
+        </table>
+        </div>
+        `)
     })
 }
 
 
 function watchForm() {
-    $('#mytable').submit(event => {
-    //clears the results
-        event.preventDefault();
-        getRemoteData();
-    });
+  $('#mytable').submit(e => {
+    e.preventDefault();
+    getRemoteData();
+  })
 }
 //run this function 1st anytime the app starts or the document is ready
 $(function () {
